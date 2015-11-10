315ea58ad62550c615a6535df11933b6786f9e41
Xit002 <Hitman400@rambler.ru>
2015-11-10 16:51:36 +0500

    Update Sost_smet.java

--- src/pack/Sost_smet.java
+++ src/pack/Sost_smet.java
@@ -97,5 +97,88 @@ public class Sost_smet {
 	}
 	
 
+		public static void smeta() {/*Класс, который доступен для любого файла из пакета pack */
+		/*  Модификатор доступа public означает, что метод виден и доступен любому классу.
+	    static означает, что не нужно создавать экземпляр(копию) объекта в памяти, чтобы использовать этот метод.
+	    void означает, что метод не возвращает данных в программу, которая его вызвала.
+	    Метод с пустыми скобками, это означает, что метод не имеет аргументов(параметров)
+	    */	
+		
+		/* Расчет материальных затрат. Если не стоит флажок Рассчитать, то материальные затраты равны 
+		 * числу которое ввел пользователь. Если флажок Рассчитать мат.затраты был включен, то мат.
+		 * затраты рассчитываются как сумма (стоимости товара умноженное на количество товара)  */
+		if (Vkl_1.calc_int==0){ //если флажок Рассчитать был выключен
+			mat_zatr_1=mat_zatr_d;
+			/* В переменную записываются данные введеные пользователем в текстовое поле которое 
+			 * расположено в файле Index. Преобразование данных в тип Double. */
+		} else {
+	        if (Vkl_2.name_1.getText().trim().length() > 0) { //если переменная не пуста
+	        	mat_zatr_1=st_1_d*kol_1_d;}
+	        if (Vkl_2.name_2.getText().trim().length() > 0) { //если переменная не пуста
+	        	mat_zatr_1=mat_zatr_1+st_2_d*kol_2_d;}
+	        if (Vkl_2.name_3.getText().trim().length() > 0) { //если переменная не пуста
+	        	mat_zatr_1=mat_zatr_1+st_3_d*kol_3_d;}
+	        if (Vkl_2.name_4.getText().trim().length() > 0) { //если переменная не пуста
+	        	mat_zatr_1=mat_zatr_1+st_4_d*kol_4_d;}
+	        if (Vkl_2.name_5.getText().trim().length() > 0) { //если переменная не пуста
+	        	mat_zatr_1=mat_zatr_1+st_5_d*kol_5_d;}
+	        if (Vkl_2.name_6.getText().trim().length() > 0) { //если переменная не пуста
+	        	mat_zatr_1=mat_zatr_1+st_6_d*kol_6_d;}
+	        if (Vkl_2.name_7.getText().trim().length() > 0) { //если переменная не пуста
+	        	mat_zatr_1=mat_zatr_1+st_7_d*kol_7_d;}
+	        if (Vkl_2.name_8.getText().trim().length() > 0) { //если переменная не пуста
+	        	mat_zatr_1=mat_zatr_1+st_8_d*kol_8_d;}
+	        if (Vkl_2.name_9.getText().trim().length() > 0) { //если переменная не пуста
+	        	mat_zatr_1=mat_zatr_1+st_9_d*kol_9_d;}
+	        if (Vkl_2.name_10.getText().trim().length() > 0) { //если переменная не пуста
+	        	mat_zatr_1=mat_zatr_1+st_10_d*kol_10_d;}
+		}
+		
+		/* Зарплата рассчитывается как Зарплата рабочего за 1 день умноженное на время изготовления изделия
+		 * и умноженное на количество рабочих котроые производят это изделие. Зарплата рабочего за день 
+		 * считается как Зарплата за месяц деленное на число рабочих дней в месяце*/
+		zarpl_1=(zarpl_d/rab_dn_d)*t_izgot_d*kol_s_d;
+		
+		/* Отчисления на социальные нужды составляют 30% от заработной платы */
+		otc_1=zarpl_1*0.3;
+		
+		/* Амортизация за месяц рассчитывается как стоимость станка деленная на срок службы станка.
+		 * Амортизация для изделия рассчитывается как амортизация за месяц деленная на 29,4(среднее число 
+		 * дней в месяце) и умноженная на количество дней изготовления изделия */
+	
+		amort_1=(st_st_d/sr_st_d)/29.4*t_izgot_d;
+				
+		/* Итого считается как сумма мат.затрат, затрат на оплату труда, отчисления, амортизация, прочие расходы */
+		
+		itogo_1=mat_zatr_1+zarpl_1+otc_1+amort_1+pr_rash_d;		
+		
+		//форматированный вывод
+	    String mat_zatr_str = String.format("%.2f", mat_zatr_1);
+	    String zarpl_str = String.format("%.2f", zarpl_1);
+	    String otc_str = String.format("%.2f", otc_1);
+	    String amort_str = String.format("%.2f", amort_1);
+	    String pr_rash_str = String.format("%.2f", pr_rash_d);
+	    String itogo_str = String.format("%.2f", itogo_1);
+
+
+	    /* String - указываем что переменная будет иметь строковый тип,
+	     * mat_zatr_str - название новой переменной,
+	     * String.format - преобразование переменной в строкой тип,
+	     	%.2f - указываем что переменная будет отображать только 2 цифры после запятой,
+	     	mat_zatr_1 - переменная типа double, которую надо преобразовать в строковый тип
+	     	 */
+		
+	    Output_smet.res_izd.setText(izd_str);
+	    Output_smet.res_mat_zatr.setText(mat_zatr_str);
+	    Output_smet.res_zarpl.setText(zarpl_str);
+	    Output_smet.res_otc.setText(otc_str);
+	    Output_smet.res_amort.setText(amort_str);
+	    Output_smet.res_pr_rash.setText(pr_rash_str);
+	    Output_smet.res_itogo.setText(itogo_str);
+
+	    /* Записываем строковые переменные в текстовые поля res_izd,res_mat_zatr .., которые расположены
+	     *  в файле Output_smet */
+		
+	}
 	
 }
